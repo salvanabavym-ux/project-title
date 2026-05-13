d3d7346d774cc9cd9a9bc02a297069c3d497efdb
salvanabavym-ux <salvanabavym@gmail.com>
2026-05-13 23:46:03 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,219 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama-cafe-1",
+      "name": "Panorama cafe 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7511195017126067,
+          "pitch": 0.04296458930800995,
+          "rotation": 1.5707963267948966,
+          "target": "1-panorama-cafe-2"
+        },
+        {
+          "yaw": -0.651724425311869,
+          "pitch": 0.03979157542043055,
+          "rotation": 4.71238898038469,
+          "target": "4-panorama-cafe-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-panorama-cafe-2",
+      "name": "Panorama cafe 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0214450051620894,
+          "pitch": 0.19286337844367019,
+          "rotation": 10.995574287564278,
+          "target": "2-panorama-cafe-3"
+        },
+        {
+          "yaw": -2.6402009994526257,
+          "pitch": 0.19949216602972086,
+          "rotation": 1.5707963267948966,
+          "target": "0-panorama-cafe-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-panorama-cafe-3",
+      "name": "Panorama cafe 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5258056865847625,
+          "pitch": 0.15666672463243891,
+          "rotation": 1.5707963267948966,
+          "target": "3-panorama-cafe-4"
+        },
+        {
+          "yaw": -0.7404751172911048,
+          "pitch": 0.22190848656759599,
+          "rotation": 4.71238898038469,
+          "target": "1-panorama-cafe-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-panorama-cafe-4",
+      "name": "Panorama cafe 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4736342453409037,
+          "pitch": 0.03144635093870818,
+          "rotation": 4.71238898038469,
+          "target": "2-panorama-cafe-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-panorama-cafe-5",
+      "name": "Panorama cafe 5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9615290463468131,
+          "pitch": 0.07215778946045504,
+          "rotation": 4.71238898038469,
+          "target": "0-panorama-cafe-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
